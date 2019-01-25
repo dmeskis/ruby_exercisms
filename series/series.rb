b7c1dfac15afd3d3af56610e2874f20dcac7e679
Mesky <dmeskis@gmail.com>
2019-01-25 09:14:20 -0700

    Updated series solution with each_cons (wonderful enumerable)

--- series/series.rb
+++ series/series.rb
@@ -7,9 +7,7 @@ class Series
 
   def slices(length)
     raise ArgumentError.new('IMPOSSIBLE!') unless @nums.length >= length
-    @nums.each_with_index.collect do |e, i|
-      @nums[i..i+length-1].join unless @nums[i..i+length].length < length
-    end.compact
+    @nums.each_cons(length).map(&:join)
   end
 
 end
