c125af7a66d9261acc572a01ef5d131b315d5aab
Mesky <dmeskis@gmail.com>
2019-02-17 11:07:44 -0700

    New iteration of raindrops

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -4,22 +4,8 @@ class Raindrops
   SOUNDS = {3 => 'Pling', 5 => 'Plang', 7 => 'Plong'}
 
   def self.convert(num)
-    raindrop_speak = factors_of(num).map { |factor, speech| SOUNDS[factor] ? SOUNDS[factor] : '' }.join
+    raindrop_speak = SOUNDS.select {|k, _| (num % k).zero? }.values.join
     raindrop_speak.empty? ? num.to_s : raindrop_speak
   end
 
-  private
-
-  def self.factors_of(num)
-    (1..Math.sqrt(num)).each_with_object([]) do |i, factors|
-      if num % i == 0
-        if num / i == i
-          factors << i
-        else
-          factors << i
-          factors << num/i
-        end
-      end
-    end
-  end
 end
