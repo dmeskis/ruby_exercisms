c553ca4a752072e61e73b068cbe11432da6b7b52
Mesky <dmeskis@gmail.com>
2019-02-17 17:22:02 -0700

    First iteration of sum of multiples

--- sum-of-multiples/sum_of_multiples.rb
+++ sum-of-multiples/sum_of_multiples.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class SumOfMultiples
+  attr_reader :multiples
+
+  def initialize(*multiples)
+    @multiples = multiples
+  end
+
+  def to(limit)
+    multiples.each_with_object([]) do |n, multiples_array|
+      multiple = n
+      until multiple >= limit || multiple.zero?
+        multiples_array << multiple
+        multiple = multiple + n
+      end
+    end.uniq.sum
+  end
 
 end
