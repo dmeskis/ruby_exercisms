a89f41a9c9f9365694d7bf13ef1d31a83fc763be
Dylan Meskis <dmeskis@gmail.com>
2019-05-19 12:28:17 -0600

    Refactoring, ruby is beautiful <3

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,24 +1,25 @@
 class Scrabble
   attr_reader :word
-  ONE = /(a|e|i|o|u|l|n|r|s|t)/i
-  TWO = /(d|g)/i
-  THREE = /(b|c|m|p)/i
-  FOUR = /(f|h|v|w|y)/i
-  FIVE = /(k)/i
-  EIGHT = /(j|x)/i
-  TEN = /(q|z)/i
-  SCORE_REGEX = {1 => ONE, 2 => TWO, 3 => THREE, 4 => FOUR, 5 => FIVE, 8 => EIGHT, 10 => TEN}
-  private_constant :ONE, :TWO, :THREE, :FOUR, :FIVE, :EIGHT, :TEN, :SCORE_REGEX
+  SCORE_REGEX = {
+    1 => /(a|e|i|o|u|l|n|r|s|t)/i,
+    2 => /(d|g)/i,
+    3 => /(b|c|m|p)/i,
+    4 => /(f|h|v|w|y)/i,
+    5 => /(k)/i,
+    8 => /(j|x)/i,
+    10 => /(q|z)/i
+  }
+  private_constant :SCORE_REGEX
 
   def initialize(word)
     @word = word.to_s
   end
 
   def score
-    Scrabble.score(word)
+    SCORE_REGEX.sum {|score, regex| word.scan(regex).count * score}
   end
 
   def self.score(word)
-    SCORE_REGEX.reduce(0) { |sum, (score, regex)| sum + word.scan(regex).count * score }
+    new(word).score
   end
 end
